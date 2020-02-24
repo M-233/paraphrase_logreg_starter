7813fe486bcc7173e499d50fed6a149bab23a009
emmerkhofer <emm228@georgetown.edu>
2020-02-24 12:00:46 -0500

    checking in lab solution

--- lab.py
+++ lab.py
@@ -1,8 +1,9 @@
 import argparse
 import numpy as np
-from util import parse_sts, preprocess_text, sts_to_pi
+from util import parse_sts, preprocess_text
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import f1_score, precision_score, recall_score
 
 def main(sts_data):
     """Transform a semantic textual similarity dataset into a paraphrase identification.
@@ -15,11 +16,25 @@ def main(sts_data):
     texts, labels = parse_sts(sts_data)
     labels = np.asarray(labels)
 
-    pi_texts, pi_labels = sts_to_pi(texts, labels)
+    # get an array of the rows where the labels are in the right interval
+    # I like this numpy
+    #pi_rows = np.where(np.logical_or(labels>=min_paraphrase, labels<=max_nonparaphrase))[0]
+    # here's a loop if you don't like numpy
+    pi_rows = [i for i,label in enumerate(labels) if label >=min_paraphrase or label<=max_nonparaphrase]
+    print(pi_rows)
+    pi_texts = [texts[i] for i in pi_rows]
+    # 1221 for dev
+    print(f"{len(pi_texts)} sentence pairs kept")
 
-    # calculate to check your split agrees with mine
-    num_nonparaphrase = 0
-    num_paraphrase = 0
+    # using indexing to get the right rows out of labels
+    pi_y = labels[pi_rows]
+    # convert to binary using threshold
+    pi_y = pi_y > max_nonparaphrase
+    print(pi_y)
+
+    # check your split agrees with mine
+    num_nonparaphrase = (pi_y == False).sum()
+    num_paraphrase = (pi_y == True).sum()
     # 957 for dev
     print(f"{num_nonparaphrase} non-paraphrase")
     # 264 for dev
@@ -27,27 +42,50 @@ def main(sts_data):
 
 
     # Instantiate a TFIDFVectorizer to create representations for sentences
+    # get a single list of texts to determine vocabulary and document frequency
+    all_t1, all_t2 = zip(*texts)
+    all_texts = all_t1 + all_t2
+    preproc_train_texts = [preprocess_text(text) for text in all_texts]
+
+    preproc_vectorizer = TfidfVectorizer("content", lowercase=True, analyzer="word",
+                                         token_pattern="\S+", use_idf=True, min_df=10)
+    preproc_vectorizer.fit(preproc_train_texts)
+
     # compute cosine similarity for each pair of sentences
     # use a threshold of 0.7 to convert each similarity score into a paraphrase prediction
     cos_sims_preproc = []
+
+    for t1,t2 in pi_texts:
+
+        t1_preproc = preprocess_text(t1)
+        t2_preproc = preprocess_text(t2)
+        pair_reprs = preproc_vectorizer.transform([t1_preproc, t2_preproc])
+        pair_similarity = cosine_similarity(pair_reprs[0], pair_reprs[1])
+        cos_sims_preproc.append(pair_similarity[0,0])
 
     predictions = np.asarray(cos_sims_preproc) > 0.7
 
 
     # calculate and print precision and recall statistics for your system
-    num_pred = 0
+    num_pred = predictions.sum()
     print(f"Number predicted paraphrase: {num_pred}")
 
-    num_pos = 0
+    num_pos = pi_y.sum()
     print(f"Number positive: {num_pos}")
 
-    num_true_pos = 0
+    true_pos = ((pi_y*1 + predictions*1) == 2)
+    num_true_pos = true_pos.sum()
     print(f"Number true positive: {num_true_pos}")
 
-    precision = 0
-    recall = 0
+    precision = num_true_pos / num_pred
+    recall = num_true_pos / num_pos
     print(f"Scores: precision {precision:0.03}\trecall {recall:0.03}")
 
+    # double check our work: use sklearn's implementation
+    p = precision_score(pi_y, predictions)
+    r = recall_score(pi_y, predictions)
+    f = f1_score(pi_y, predictions)
+    print(f"Scores: precision {p:0.03}\trecall {r:0.03}\tf1 {f:0.03}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- util.py
+++ util.py
@@ -1,10 +1,20 @@
+import numpy as np
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+import string
+from nltk.tokenize import word_tokenize
 
 def preprocess_text(text):
     """Preprocess one sentence: tokenizes, lowercases, applies the Porter stemmer,
      removes punctuation tokens and stopwords.
      Returns a string of tokens joined by whitespace."""
-
-    return text
+    stemmer = PorterStemmer()
+    stops = set(stopwords.words('english'))
+    toks = word_tokenize(text)
+    toks_stemmed = [stemmer.stem(tok.lower()) for tok in toks]
+    toks_nopunc = [tok for tok in toks_stemmed if tok not in string.punctuation]
+    toks_nostop = [tok for tok in toks_nopunc if tok not in stops]
+    return " ".join(toks_nostop)
 
 # TODO: lab, homework
 def parse_sts(data_file):
@@ -16,11 +26,17 @@ def parse_sts(data_file):
     texts = []
     labels = []
 
+    with open(data_file, 'r') as dd:
+        for line in dd:
+            fields = line.strip().split("\t")
+            labels.append(float(fields[4]))
+            t1 = fields[5].lower()
+            t2 = fields[6].lower()
+            texts.append((t1, t2))
     return texts, labels
 
 # TODO: lab, homework
 def sts_to_pi(texts, sts_labels, max_nonparaphrase, min_paraphrase):
     """Convert a dataset from semantic textual similarity to paraphrase.
-    Remove any examples that are > max_nonparaphrase and < min_nonparaphrase."""
-
+    Remove any examples that are > max_nonparaphrase and < min_paraphrase."""
     return texts, sts_labels
